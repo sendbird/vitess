be2d4bb1be3dbed0a8364da3c8c871a4677ac0df
Toliver Jue <toliver@planetscale.com>
2019-06-10 23:15:18 +0900

    enable query
    Signed-off-by: Toliver Jue <toliver@planetscale.com>

--- examples/local/vtctld-up.sh
+++ examples/local/vtctld-up.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Copyright 2017 Google Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,6 +40,7 @@ $VTROOT/bin/vtctld \
   -web_dir2 $VTTOP/web/vtctld2/app \
   -workflow_manager_init \
   -workflow_manager_use_election \
+  -enable_queries \
   -service_map 'grpc-vtctl' \
   -backup_storage_implementation file \
   -file_backup_storage_root $VTDATAROOT/backups \
