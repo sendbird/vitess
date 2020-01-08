e66af366800bc28a5bdb01f580c819e5d637e15f
Arindam Nayak <arindam.nayak@outlook.com>
2020-01-08 13:45:28 +0530

    - clean test files used for instr - used sonar token from secret Signed-off-by: Arindam Nayak <arindam.nayak@outlook.com>

--- tools/all_test_for_coverage.sh
+++ tools/all_test_for_coverage.sh
@@ -21,23 +21,11 @@
 # Output per line: <full Go package name> <all _test.go files in the package>*
 
 
-### Execute go/test/endtoend testcase ###
-source build.env
-
-packages_with_tests=$(go list -f '{{if len .TestGoFiles}}{{.ImportPath}} {{join .TestGoFiles " "}}{{end}}' ./go/.../endtoend/... | sort)
-
-cluster_tests=$(echo "$packages_with_tests" | grep -E "go/test/endtoend" | cut -d" " -f1)
-
-# Run cluster test sequentially
-
-for i in $cluster_tests
-do
-   echo "starting test for $i"
-   go test  $i -v -p=1 -is-coverage=true || :
-done
-
 ### Execute unit testcase ###
-
+source build.env
+make tools
+make build
+echo "--------- executing unit testcases ---------"
 packages_with_all_tests=$(go list -f '{{if len .TestGoFiles}}{{.ImportPath}} {{join .TestGoFiles " "}}{{end}}' ./go/... | sort)
 all_except_endtoend_tests=$(echo "$packages_with_all_tests" | grep -v "endtoend" | cut -d" " -f1 )
 
@@ -47,3 +35,34 @@ do
    go test -coverpkg=vitess.io/vitess/go/... -coverprofile "/tmp/unit_$counter.out" $pkg -v -p=1 || :
    counter=$((counter+1))
 done
+
+### Copy the test files to get instrumented binaries ###
+cp ./tools/coverage-go/vtctl_test.go ./go/cmd/vtctl/vtctl_test.go
+cp ./tools/coverage-go/vtctld_test.go ./go/cmd/vtctld/vtctld_test.go
+cp ./tools/coverage-go/mysqlctl_test.go ./go/cmd/mysqlctl/mysqlctl_test.go
+cp ./tools/coverage-go/vtctlclient_test.go ./go/cmd/vtctlclient/vtctlclient_test.go
+cp ./tools/coverage-go/vttablet_test.go ./go/cmd/vttablet/vttablet_test.go
+cp ./tools/coverage-go/vtgate_test.go ./go/cmd/vtgate/vtgate_test.go
+cp ./tools/coverage-go/vtworker_test.go ./go/cmd/vtworker/vtworker_test.go
+cp ./tools/coverage-go/vtworkerclient_test.go ./go/cmd/vtworkerclient/vtworkerclient_test.go
+
+go test -coverpkg=vitess.io/vitess/go/...  -c vitess.io/vitess/go/cmd/vtctl -o ./bin/vtctl
+go test -coverpkg=vitess.io/vitess/go/...  -c vitess.io/vitess/go/cmd/vtctld -o ./bin/vtctld
+go test -coverpkg=vitess.io/vitess/go/...  -c vitess.io/vitess/go/cmd/mysqlctl -o ./bin/mysqlctl
+go test -coverpkg=vitess.io/vitess/go/...  -c vitess.io/vitess/go/cmd/vtctlclient -o ./bin/vtctlclient
+go test -coverpkg=vitess.io/vitess/go/...  -c vitess.io/vitess/go/cmd/vttablet -o ./bin/vttablet
+go test -coverpkg=vitess.io/vitess/go/...  -c vitess.io/vitess/go/cmd/vtgate -o ./bin/vtgate
+go test -coverpkg=vitess.io/vitess/go/...  -c vitess.io/vitess/go/cmd/vtworker -o ./bin/vtworker
+go test -coverpkg=vitess.io/vitess/go/...  -c vitess.io/vitess/go/cmd/vtworkerclient -o ./bin/vtworkerclient
+
+### Execute go/test/endtoend testcase ###
+echo "--------- executing endtoend testcases ---------"
+cluster_tests=$(echo "$packages_with_all_tests" | grep -E "go/test/endtoend" | cut -d" " -f1)
+
+# Run cluster test sequentially
+for i in $cluster_tests
+do
+   echo "starting test for $i"
+   go test  $i -v -p=1 -is-coverage=true || :
+done
+
