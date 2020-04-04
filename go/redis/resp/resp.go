3e0258b24ad837905b1531c0a61a6d1a7c8a226b
Toliver Jue <toliver@planetscale.com>
2020-04-04 23:05:54 +0900

    int

--- go/redis/resp/resp.go
+++ go/redis/resp/resp.go
@@ -16,7 +16,10 @@ var (
 
 	simplePrefix = []byte("+")
 	bulkPrefix   = []byte("$")
+	intPrefix    = []byte(":")
+	errorPrefix  = []byte("-")
 	crlf         = []byte("\r\n")
+	terminator   = crlf
 )
 
 func SimpleString(buf []byte) []byte {
@@ -24,7 +27,7 @@ func SimpleString(buf []byte) []byte {
 	return bytes.Join([][]byte{
 		simplePrefix,
 		buf,
-		crlf,
+		terminator,
 	}, nil)
 }
 
@@ -36,6 +39,24 @@ func BulkString(buf []byte) []byte {
 		[]byte(strconv.Itoa(len(buf))),
 		crlf,
 		buf,
-		crlf,
+		terminator,
+	}, nil)
+}
+
+func Int64(i int64) []byte {
+	// Format: ":0\r\n"
+	return bytes.Join([][]byte{
+		intPrefix,
+		[]byte(strconv.FormatInt(i, 10)),
+		terminator,
+	}, nil)
+}
+
+func Error(msg string) []byte {
+	// Format: "-Error message\r\n"
+	return bytes.Join([][]byte{
+		errorPrefix,
+		[]byte(msg),
+		terminator,
 	}, nil)
 }
--- go/redis/resp/resp_test.go
+++ go/redis/resp/resp_test.go
@@ -50,3 +50,47 @@ func TestBulkString(t *testing.T) {
 		utils.MustMatch(t, wantOut, out, "out doesn't match")
 	}
 }
+
+func TestInt64(t *testing.T) {
+	tcs := []struct {
+		in  int64
+		out string
+	}{
+		{
+			in:  0,
+			out: ":0\r\n",
+		},
+		{
+			in:  1000,
+			out: ":1000\r\n",
+		},
+	}
+
+	for _, tc := range tcs {
+		wantOut := []byte(tc.out)
+		out := resp.Int64(tc.in)
+		utils.MustMatch(t, wantOut, out, "out doesn't match")
+	}
+}
+
+func TestError(t *testing.T) {
+	tcs := []struct {
+		in  string
+		out string
+	}{
+		{
+			in:  "",
+			out: "-\r\n",
+		},
+		{
+			in:  "Error",
+			out: "-Error\r\n",
+		},
+	}
+
+	for _, tc := range tcs {
+		wantOut := []byte(tc.out)
+		out := resp.Error(tc.in)
+		utils.MustMatch(t, wantOut, out, "out doesn't match")
+	}
+}
