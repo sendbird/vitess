658bd322e43d1111efa5529af9bfa287d36c2977
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-06-30 12:15:00 +0300

    improved test, more clarity
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/test/endtoend/onlineddl/vrepl_suite/testdata/alter-charset-non-utf8/create.sql
+++ go/test/endtoend/onlineddl/vrepl_suite/testdata/alter-charset-non-utf8/create.sql
@@ -1,16 +1,16 @@
 drop table if exists onlineddl_test;
 create table onlineddl_test (
   id int auto_increment,
-  t1 varchar(128)  charset latin1 collate latin1_swedish_ci,
-  t2 varchar(128)  charset latin1 collate latin1_swedish_ci,
-  tutf8 varchar(128) charset utf8,
-  tutf8mb4 varchar(128) charset utf8mb4,
-  tlatin1 varchar(128)  charset latin1 collate latin1_swedish_ci,
+  latin1_to_utf8mb4 varchar(128) charset latin1 collate latin1_swedish_ci,
+  latin1_to_latin2 varchar(128) charset latin1 collate latin1_swedish_ci,
+  utf8_to_latin1 varchar(128) charset utf8,
+  keep_utf8mb4 varchar(128) charset utf8mb4,
+  keep_latin1 varchar(128) charset latin1 collate latin1_swedish_ci,
   primary key(id)
 ) auto_increment=1;
 
 insert into onlineddl_test values (null, md5(rand()), md5(rand()), md5(rand()), md5(rand()), md5(rand()));
-insert into onlineddl_test values (null, 'átesting', 'átesting', 'átesting', 'átesting', 'átesting');
+insert into onlineddl_test values (null, 'átesting-l1toutf8mb4-vcopier', 'átesting-l1tol2-vcopier', 'átesting-utf8tol1-vcopier', 'átesting-utf8mb4-vcopier', 'átesting-l1-vcopier');
 
 drop event if exists onlineddl_test;
 delimiter ;;
@@ -23,6 +23,6 @@ create event onlineddl_test
   do
 begin
   insert into onlineddl_test values (null, md5(rand()), md5(rand()), md5(rand()), md5(rand()), md5(rand()));
-  insert into onlineddl_test values (null, 'átesting-binlog', 'átesting-binlog', 'átesting-binlog', 'átesting-binlog', 'átesting-binlog');
-  insert into onlineddl_test values (null, 'átesting-bnull', 'átesting-bnull', 'átesting-bnull', null, null);
+  insert into onlineddl_test values (null, 'átesting-l1toutf8mb4-binlog', 'átesting-l1tol2-binlog', 'átesting-utf8tol1-binlog', 'átesting-utf8mb4-binlog', 'átesting-l1-binlog');
+  insert into onlineddl_test values (null, 'átesting-l1toutf8mb4-bnull', 'átesting-l1tol2-bnull', 'átesting-utf8tol1-bnull', null, null);
 end ;;
